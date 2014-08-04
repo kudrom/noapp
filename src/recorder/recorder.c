859b5beab1015008b12e17703338f5aa10ff6007
kudrom <kudrom@riseup.net>
2014-08-04 02:41:44 +0200

    Added context for the recorder

--- src/recorder/recorder.c
+++ src/recorder/recorder.c
@@ -2,21 +2,27 @@
 #include <pulse/pulseaudio.h>
 #include <errno.h>
 #include <string.h>
+#include "recorder.h"
 
-static FILE *g_recording_file = NULL;
+struct recorder_context{
+    char *filename;
+    FILE *recording_file;
+    int pa_ready;
+};
+typedef struct recorder_context recorder_context_t;
 
 void pa_state_cb(pa_context *ctx, void *userdata){
     pa_context_state_t state;
-    int *pa_ready = userdata;
+    recorder_context_t *rctx = (recorder_context_t *) userdata;
     state = pa_context_get_state(ctx);
 
     switch (state){
         case PA_CONTEXT_FAILED:
         case PA_CONTEXT_TERMINATED:
-            *pa_ready = 2;
+            rctx->pa_ready = 2;
             break;
         case PA_CONTEXT_READY:
-            *pa_ready = 1;
+            rctx->pa_ready = 1;
             break;
         case PA_CONTEXT_UNCONNECTED:
         case PA_CONTEXT_CONNECTING:
@@ -30,13 +36,26 @@ void pa_state_cb(pa_context *ctx, void *userdata){
 static void stream_request_cb(pa_stream *s, size_t length, void *userdata){
     const void *data;
     size_t nbytes = 0;
-    pa_mainloop *pa_ml = (pa_mainloop *) userdata;
+    recorder_context_t *rctx = (recorder_context_t *) userdata;
     pa_stream_peek(s, &data, &nbytes);
-    fwrite(data, sizeof(uint8_t), nbytes, g_recording_file);
-    pa_stream_drop(s);
+    fwrite(data, sizeof(uint8_t), nbytes, rctx->recording_file);
+    if (data)
+        pa_stream_drop(s);
 }
 
-int init(char *filename){
+int init_filename(recorder_context_t *rctx){
+    int retval = 0;
+
+    if ((rctx->recording_file = fopen(rctx->filename, "wb")) < 0){
+        fprintf(stderr, "Failed to open the file for recording.\n");
+        fprintf(stderr, "\terrno: %s\n", strerror(errno));
+        retval = -1;
+    }
+
+    return retval;
+}
+
+int init_recorder(char *filename){
     pa_mainloop *pa_ml = NULL;
     pa_mainloop_api *pa_mlapi = NULL;
     pa_context *pa_ctx = NULL;
@@ -45,12 +64,13 @@ int init(char *filename){
     pa_proplist *stream_properties = pa_proplist_new();
     pa_sample_spec pa_ss;
     int retval = 0;
-    int pa_ready = 0;
+    recorder_context_t rctx = {
+        .filename = filename,
+        .pa_ready = 0
+    };
 
-    if ((g_recording_file = fopen(filename, "wb")) < 0){
-        fprintf(stderr, "Failed to open the file for recording.\n");
-        fprintf(stderr, "\terrno: %s\n", strerror(errno));
-        retval = -1;
+    if((retval = init_filename(&rctx)) < 0){
+        fprintf(stderr, "Failed to init filename.\n");
         goto exit;
     }
 
@@ -59,11 +79,11 @@ int init(char *filename){
     pa_ctx = pa_context_new_with_proplist(pa_mlapi, "NoApp recorder", ctx_properties);
     pa_context_connect(pa_ctx, NULL, 0, NULL);
 
-    pa_context_set_state_callback(pa_ctx, pa_state_cb, &pa_ready);
-    while (pa_ready == 0){
+    pa_context_set_state_callback(pa_ctx, pa_state_cb, &rctx);
+    while (rctx.pa_ready == 0){
         pa_mainloop_iterate(pa_ml, 1, NULL);
     }
-    if (pa_ready == 2){
+    if (rctx.pa_ready == 2){
         retval = -1;
         goto exit;
     }
@@ -72,13 +92,11 @@ int init(char *filename){
     pa_ss.channels = 1;
     pa_ss.format = PA_SAMPLE_S16LE;
     recording_stream = pa_stream_new_with_proplist(pa_ctx, "NoApp recorder", &pa_ss, NULL, stream_properties);
-    pa_stream_set_read_callback(recording_stream, stream_request_cb, pa_ml);
+    pa_stream_set_read_callback(recording_stream, stream_request_cb, &rctx);
 
-    int r;
-    r = pa_stream_connect_record(recording_stream, NULL, NULL, 0);
-    if (r < 0){
+    retval = pa_stream_connect_record(recording_stream, NULL, NULL, 0);
+    if (retval < 0){
         printf("pa_stream_connect_playback failed\n");
-        retval = -1;
         goto exit;
     }
 
@@ -86,8 +104,8 @@ int init(char *filename){
     printf("retval: %d\n", retval);
 
 exit:
-    if (g_recording_file)
-        fclose(g_recording_file);
+    if (rctx.recording_file)
+        fclose(rctx.recording_file);
     pa_context_disconnect(pa_ctx);
     pa_context_unref(pa_ctx);
     pa_mainloop_free(pa_ml);
@@ -98,6 +116,6 @@ exit:
 int main(int argc, char*argv[])
 {
     char *filename = argc == 1 ? "/tmp/noapp_recording.pcm" : argv[1];
-    return init(filename);
+    return init_recorder(filename);
 }
 #endif

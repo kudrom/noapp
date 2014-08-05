2cea6e9784c794bad0b7bdfc904b44bd07660991
kudrom <kudrom@riseup.net>
2014-08-05 17:05:32 +0200

    Refactoring to add space to the following features.

--- includes/recorder.h
+++ includes/recorder.h
@@ -1,8 +1,61 @@
+#include <pulse/pulseaudio.h>
+
+/*
+ * Main structure of the recorder subsystem.
+ */
+struct recorder_context{
+    char *filename;
+    FILE *recording_file;
+    int pa_ready;
+    bool mute;
+    pa_mainloop *pa_ml;
+    pa_context *pa_ctx;
+    pa_stream *recording_stream;
+    pa_sample_spec pa_ss;
+};
+typedef struct recorder_context recorder_context_t;
+
 // TODO: Add a parameter to the next stage?
-int init_recorder(char *filename);
+/*
+ * To use the NoApp recorder first you must initialize a correct recording_context
+ * and then you can start recording with a call to start_recording(rctx);
+ */
+recorder_context_t *init_recorder_context(char *filename);
 
-void change_recorder_filename(char *filename);
+/*
+ * Start recording the utterances that the user spells to the system.
+ */
+int start_recording(recorder_context_t *rctx);
 
-void bypass_next_stage();
+/*
+ *  Stop recording the utterances that the user spells to the system.
+ *  It must be called to free memory, it desconnects to pulseAudio.
+ */
+int stop_recording(recorder_context_t *rctx);
 
-void mute_recorder();
+/*
+ * Changes the target of the recording to the new_filename
+ */
+int change_recorder_filename(recorder_context_t *rctx, char *new_filename);
+
+/*
+ * Stop recording the utterances that the user spells to the system.
+ * It doesn't free memory, it is safe to call it multiple times.
+ */
+int mute_recorder(recorder_context_t *rctx);
+
+/*
+ * Activates the recorder, it is safe to call it multiple times.
+ */
+int unmute_recorder(recorder_context_t *rctx);
+
+/*
+ * Mutes/Unmutes the recorder.
+ */
+int toggle_recorder(recorder_context_t *rctx);
+
+
+
+
+int bypass_next_stage();
+
--- src/recorder/recorder.c
+++ src/recorder/recorder.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-#include <pulse/pulseaudio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 #include "recorder.h"
 
-struct recorder_context{
-    char *filename;
-    FILE *recording_file;
-    int pa_ready;
-};
-typedef struct recorder_context recorder_context_t;
-
-void pa_state_cb(pa_context *ctx, void *userdata){
+static void pa_state_cb(pa_context *ctx, void *userdata){
     pa_context_state_t state;
     recorder_context_t *rctx = (recorder_context_t *) userdata;
     state = pa_context_get_state(ctx);
@@ -19,6 +12,7 @@ void pa_state_cb(pa_context *ctx, void *userdata){
     switch (state){
         case PA_CONTEXT_FAILED:
         case PA_CONTEXT_TERMINATED:
+            fprintf(stderr, "State callback of pulseAudio return failure in its context");
             rctx->pa_ready = 2;
             break;
         case PA_CONTEXT_READY:
@@ -43,7 +37,41 @@ static void stream_request_cb(pa_stream *s, size_t length, void *userdata){
         pa_stream_drop(s);
 }
 
-int init_filename(recorder_context_t *rctx){
+static int init_pa(recorder_context_t *rctx){
+    pa_mainloop_api *pa_mlapi;
+    pa_proplist *ctx_properties = pa_proplist_new();
+    pa_proplist *stream_properties = pa_proplist_new();
+    int retval = 0;
+
+    rctx->pa_ml = pa_mainloop_new();
+    pa_mlapi = pa_mainloop_get_api(rctx->pa_ml);
+    rctx->pa_ctx = pa_context_new_with_proplist(pa_mlapi, "NoApp recorder", ctx_properties);
+    pa_context_connect(rctx->pa_ctx, NULL, 0, NULL);
+
+    pa_context_set_state_callback(rctx->pa_ctx, pa_state_cb, rctx);
+    while (rctx->pa_ready == 0){
+        pa_mainloop_iterate(rctx->pa_ml, 1, NULL);
+    }
+    if (rctx->pa_ready == 2){
+        retval = -1;
+        goto exit;
+    }
+
+    rctx->recording_stream = pa_stream_new_with_proplist(rctx->pa_ctx, "NoApp recorder", &rctx->pa_ss, NULL, stream_properties);
+    pa_stream_set_read_callback(rctx->recording_stream, stream_request_cb, rctx);
+
+    retval = pa_stream_connect_record(rctx->recording_stream, NULL, NULL, 0);
+    if (retval < 0){
+        printf("pa_stream_connect_playback failed\n");
+        goto exit;
+    }
+
+exit:
+    return retval;
+}
+
+
+static int init_filename(recorder_context_t *rctx){
     int retval = 0;
 
     if ((rctx->recording_file = fopen(rctx->filename, "wb")) < 0){
@@ -55,60 +83,47 @@ int init_filename(recorder_context_t *rctx){
     return retval;
 }
 
-int init_recorder(char *filename){
-    pa_mainloop *pa_ml = NULL;
-    pa_mainloop_api *pa_mlapi = NULL;
-    pa_context *pa_ctx = NULL;
-    pa_stream *recording_stream = NULL;
-    pa_proplist *ctx_properties = pa_proplist_new();
-    pa_proplist *stream_properties = pa_proplist_new();
-    pa_sample_spec pa_ss;
+recorder_context_t *init_recorder_context(char *filename){
+    recorder_context_t *retval;
+    retval = malloc(sizeof(recorder_context_t));
+    retval->filename = filename;
+    retval->pa_ready = 0;
+    retval->pa_ss.rate = 44100;
+    retval->pa_ss.channels = 2;
+    retval->pa_ss.format = PA_SAMPLE_S16LE;
+
+    return retval;
+}
+
+int start_recording(recorder_context_t *rctx){
     int retval = 0;
-    recorder_context_t rctx = {
-        .filename = filename,
-        .pa_ready = 0
-    };
 
-    if((retval = init_filename(&rctx)) < 0){
-        fprintf(stderr, "Failed to init filename.\n");
+    if ((retval = init_filename(rctx))){
+        fprintf(stderr, "Failed in the initialization of the recording file");
         goto exit;
     }
 
-    pa_ml = pa_mainloop_new();
-    pa_mlapi = pa_mainloop_get_api(pa_ml);
-    pa_ctx = pa_context_new_with_proplist(pa_mlapi, "NoApp recorder", ctx_properties);
-    pa_context_connect(pa_ctx, NULL, 0, NULL);
-
-    pa_context_set_state_callback(pa_ctx, pa_state_cb, &rctx);
-    while (rctx.pa_ready == 0){
-        pa_mainloop_iterate(pa_ml, 1, NULL);
-    }
-    if (rctx.pa_ready == 2){
-        retval = -1;
+    if ((retval = init_pa(rctx))){
+        fprintf(stderr, "Failed in the initializaion of pulse audio");
         goto exit;
     }
 
-    pa_ss.rate = 44100;
-    pa_ss.channels = 1;
-    pa_ss.format = PA_SAMPLE_S16LE;
-    recording_stream = pa_stream_new_with_proplist(pa_ctx, "NoApp recorder", &pa_ss, NULL, stream_properties);
-    pa_stream_set_read_callback(recording_stream, stream_request_cb, &rctx);
-
-    retval = pa_stream_connect_record(recording_stream, NULL, NULL, 0);
-    if (retval < 0){
-        printf("pa_stream_connect_playback failed\n");
-        goto exit;
-    }
-
-    pa_mainloop_run(pa_ml, &retval);
-    printf("retval: %d\n", retval);
+    pa_mainloop_run(rctx->pa_ml, &retval);
 
 exit:
-    if (rctx.recording_file)
-        fclose(rctx.recording_file);
-    pa_context_disconnect(pa_ctx);
-    pa_context_unref(pa_ctx);
-    pa_mainloop_free(pa_ml);
+    stop_recording(rctx);
+    return retval;
+}
+
+int stop_recording(recorder_context_t *rctx){
+    int retval = 0;
+
+    if (rctx->recording_file)
+        fclose(rctx->recording_file);
+    pa_context_disconnect(rctx->pa_ctx);
+    pa_context_unref(rctx->pa_ctx);
+    pa_mainloop_free(rctx->pa_ml);
+    free(rctx);
     return retval;
 }
 
@@ -116,6 +131,8 @@ exit:
 int main(int argc, char*argv[])
 {
     char *filename = argc == 1 ? "/tmp/noapp_recording.pcm" : argv[1];
-    return init_recorder(filename);
+    recorder_context_t *rctx;
+    rctx = init_recorder_context(filename);
+    return start_recording(rctx);
 }
 #endif
